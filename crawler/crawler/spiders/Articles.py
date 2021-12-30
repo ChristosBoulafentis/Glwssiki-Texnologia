02a2402a8c65b4a46dca09f5276b5e08b96beba1
ChristosBoulafentis <82766346+ChristosBoulafentis@users.noreply.github.com>
2021-12-30 02:56:31 +0200

    new

---
+++ crawler/crawler/spiders/Articles.py
@@ -0,0 +1,26 @@
+# Spider 1 
+# Articles.py which scrape article links# imports
+import scrapy
+from scrapy.http import Request
+
+class ArticlesSpider(scrapy.Spider):
+    name = 'Articles'
+    allowed_domains = ['bbc.com']
+    start_urls = ['https://www.bbc.com/']
+    
+    def start_requests(self):
+        url="https://www.bbc.com/news/world-{}"
+        link_urls = [url.format(i) for i in range(59800000,59814662)]# Loops through 500 pages to get the article links
+        for link_url in link_urls:
+            print(link_url)# Request to get the HTML content
+            request=Request(link_url, cookies={'store_language':'en'}, 
+            callback=self.parse_main_pages)
+            yield request
+    
+    def parse_main_pages(self,response):
+        filename = response.url.split("/")[-1] + '.html'
+        with open('artcl/ ' + filename, 'wb') as f:
+            f.write(response.body)
+            
+    def parse(self, response):
+        pass
---
+++ crawler/crawler/spiders/Clean_html.py
@@ -0,0 +1,29 @@
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import os
+
+read_directory = '/home/chris/Documents/GitHub/Glwssiki-Texnologia/crawler/crawler/spiders/artcl/'
+write_directory = '/home/chris/Documents/GitHub/Glwssiki-Texnologia/crawler/crawler/spiders/cleared/'
+for filename in os.listdir(read_directory):
+    f = open(read_directory + filename, "r")
+
+    soup = BeautifulSoup(f, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+        # get text
+        text = soup.get_text()
+
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in text.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    #print(text)
+    with open(write_directory + filename + '.txt', 'w') as r:
+        r.write(text)
